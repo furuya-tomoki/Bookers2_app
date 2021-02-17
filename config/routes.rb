9d9a763bd7bddfd693f36eb4b2aba5232d009439
EC2 Default User <ec2-user@ip-172-31-89-100.ec2.internal>
2021-02-17 06:50:08 +0000

    [Finish]Bookers2

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+
+  root 'homes#top'
+  get '/home/about' => 'homes#about'
+
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
+end
